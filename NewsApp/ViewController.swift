3b0fa57a97f8801436166f3106f0c8aef7cabced
DHRUV SHRIVASTAVA <dhruvshrivastava18@gmail.com>
2022-08-07 15:38:50 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Dhruv Shrivastava on 07/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
